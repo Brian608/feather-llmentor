c4c23a33178c36756ee2631045af8cd0251df9a8
feather <Brian608@163.com>
2026-05-01 15:48:32 +0800

    【feat】mcp-server-sse

---
+++ mcp/mcp-server-sse/src/main/java/org/feather/llm/mcp/McpServerSseApplication.java
@@ -0,0 +1,19 @@
+package org.feather.llm.mcp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: feather-llmentor
+ * @package: org.feather.llm.mcp
+ * @className: McpServerSseApplication
+ * @author: feather
+ * @description:
+ * @since: 2026-05-01 3:19 PM
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class McpServerSseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(McpServerSseApplication.class, args);
+    }
+}
