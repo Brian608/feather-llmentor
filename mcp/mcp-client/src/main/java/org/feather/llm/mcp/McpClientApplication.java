5426eb3dc1162d9a8efb85e09ce80d9364903bea
feather <Brian608@163.com>
2026-05-07 15:50:43 +0800

    【feat】mcp-client

---
+++ mcp/mcp-client/src/main/java/org/feather/llm/mcp/McpClientApplication.java
@@ -0,0 +1,20 @@
+package org.feather.llm.mcp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @projectName: feather-llmentor
+ * @package: org.feather.llm.mcp
+ * @className: McpClientApplication
+ * @author: feather
+ * @description:
+ * @since: 2026-05-07 3:45 PM
+ * @version: 1.0
+ */
+@EnableScheduling
+@SpringBootApplication
+public class McpClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(McpClientApplication.class, args);
+    }
+
+}
